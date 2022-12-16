5a4922479ac4a5b2a45ec2eec6f7a460805dc1ed
bath0ry <plylegacy@gmail.com>
2022-12-16 19:54:07 -0300

    add const in all

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SafeArea(child: ItemCartInhertied(child: HomePage())),
+      home: SafeArea(child: ItemCartInhertied(child: const HomePage())),
     );
   }
 }
